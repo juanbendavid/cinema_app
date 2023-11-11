208b9c77c407b8eb4e4703fe4727c2f1e206e3da
Juan David <juanemanueldavidzaracho@gmail.com>
2023-11-11 13:35:00 -0300

    enviroment

---
+++ lib/domain/datasources/movie_datasource.dart
@@ -0,0 +1,6 @@
+import 'package:cinema_app/domain/entities/movie.dart';
+
+abstract class MovieDataSource{
+  Future<List<Movie>> getNowPlaying({int page =1});
+  
+}
---
+++ lib/domain/repositories/movies_repository.dart
@@ -0,0 +1,6 @@
+import 'package:cinema_app/domain/entities/movie.dart';
+
+abstract class MovieRepository {
+  Future<List<Movie>> getNowPlaying({int page =1});
+  
+}
