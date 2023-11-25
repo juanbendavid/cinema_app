f49e6d17c37aa8f4090f230c6cd096ba1d7a47f0
Juan David <juanemanueldavidzaracho@gmail.com>
2023-11-25 19:07:54 -0300

    horizontal slide movies

--- lib/presentations/widgets/movies/movie_horizontal_listview.dart
+++ lib/presentations/widgets/movies/movie_horizontal_listview.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 import '../../../domain/entities/movie.dart';
@@ -22,7 +23,65 @@ class MovieHorizontalListview extends StatelessWidget {
       child: Column(
         children: [
           if (title != null || subTitle != null)
-            _Title(title: title!, subTitle: subTitle!)
+            _Title(title: title!, subTitle: subTitle!),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: movies.length,
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: ((context, index) {
+                    return _Slide(movie: movies[index]);
+                  })))
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+  const _Slide({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //* IMAGEN
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                loadingBuilder: (context, child, loadingProgress) {
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ),
+          const SizedBox(height: 5),
+          // *TITLE
+          SizedBox(
+            width: 150,
+            child: Text(movie.title, maxLines: 2, style: textStyle.titleSmall,),
+
+          ),
+          // * RATING
+          Row(
+            children: [
+              Icon(Icons.star_half, color: Colors.yellow.shade800,),
+              const SizedBox(width: 3,),
+              Text(movie.voteAverage.toString(), style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+              SizedBox(width: 10),
+              Text(movie.popularity.toString(), style: textStyle.bodySmall,)
+            ],
+          ),
         ],
       ),
     );
@@ -45,9 +104,9 @@ class _Title extends StatelessWidget {
           Text(title, style: textStyle),
           const Spacer(),
           FilledButton.tonal(
-            style: const ButtonStyle(visualDensity: VisualDensity.compact),
-            onPressed: () {}, child: Text(subTitle))
-            
+              style: const ButtonStyle(visualDensity: VisualDensity.compact),
+              onPressed: () {},
+              child: Text(subTitle))
         ],
       ),
     );
