27417e64702bea39e367c23050967d25e5b0ede4
Juan David <juanemanueldavidzaracho@gmail.com>
2023-12-18 20:35:27 -0300

    poster movie screen

--- lib/presentations/screens/movies/movie_screen.dart
+++ lib/presentations/screens/movies/movie_screen.dart
@@ -19,17 +19,76 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
   }
+
   @override
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
 
-    if (movie==null) {
-      return const Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 2,),),);
+    if (movie == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        ),
+      );
     }
 
-    return  Scaffold(
-      appBar: AppBar(),
-      body: Center(child: Text(movie.id.toString()),),
+    return Scaffold(
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [_CustomSliverAppBar(movie: movie)],
+      ),
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+  final Movie movie;
+  const _CustomSliverAppBar({super.key, required this.movie});
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(
+          children: [
+            SizedBox.expand(
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox.expand(
+                child: DecoratedBox(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.7, 1.0],
+                      colors: [Colors.transparent, Colors.black])),
+            )),
+            const SizedBox.expand(
+                child: DecoratedBox(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      stops: [0.0, 0.3],
+                      colors: [Colors.black,
+                       Colors.transparent])),
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
